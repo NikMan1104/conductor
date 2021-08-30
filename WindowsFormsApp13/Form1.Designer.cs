ea7029629211d209694060ec9c2543aee5365398
NikMan1104 <73939052+NikMan1104@users.noreply.github.com>
2021-08-30 16:35:48 +0300

    conductor

---
+++ WindowsFormsApp13/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿
+namespace WindowsFormsApp13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBoxFileItem = new System.Windows.Forms.RichTextBox();
+            this.treeView = new System.Windows.Forms.TreeView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.menu = new System.Windows.Forms.ToolStripSplitButton();
+            this.свернутьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.развернутьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.закрытьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.buttonChangeOrAdd = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // richTextBoxFileItem
+            // 
+            this.richTextBoxFileItem.Location = new System.Drawing.Point(453, 59);
+            this.richTextBoxFileItem.Name = "richTextBoxFileItem";
+            this.richTextBoxFileItem.Size = new System.Drawing.Size(186, 371);
+            this.richTextBoxFileItem.TabIndex = 0;
+            this.richTextBoxFileItem.Text = "";
+            // 
+            // treeView
+            // 
+            this.treeView.Location = new System.Drawing.Point(12, 59);
+            this.treeView.Name = "treeView";
+            this.treeView.Size = new System.Drawing.Size(185, 371);
+            this.treeView.TabIndex = 1;
+            this.treeView.AfterExpand += new System.Windows.Forms.TreeViewEventHandler(this.treeView_AfterExpand);
+            this.treeView.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.treeView_MouseDoubleClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(70, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Диски:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(450, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(145, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Данные файла:";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.menu});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(651, 25);
+            this.toolStrip1.TabIndex = 4;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // menu
+            // 
+            this.menu.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.menu.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.свернутьToolStripMenuItem,
+            this.развернутьToolStripMenuItem,
+            this.закрытьToolStripMenuItem});
+            this.menu.Image = global::WindowsFormsApp13.Properties.Resources.icons8_проводник_windows_50;
+            this.menu.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.menu.Name = "menu";
+            this.menu.Size = new System.Drawing.Size(32, 22);
+            this.menu.Text = "toolStripSplitButton1";
+            // 
+            // свернутьToolStripMenuItem
+            // 
+            this.свернутьToolStripMenuItem.Name = "свернутьToolStripMenuItem";
+            this.свернутьToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.свернутьToolStripMenuItem.Text = "Свернуть";
+            this.свернутьToolStripMenuItem.Click += new System.EventHandler(this.свернутьToolStripMenuItem_Click);
+            // 
+            // развернутьToolStripMenuItem
+            // 
+            this.развернутьToolStripMenuItem.Name = "развернутьToolStripMenuItem";
+            this.развернутьToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.развернутьToolStripMenuItem.Text = "Развернуть";
+            this.развернутьToolStripMenuItem.Click += new System.EventHandler(this.развернутьToolStripMenuItem_Click);
+            // 
+            // закрытьToolStripMenuItem
+            // 
+            this.закрытьToolStripMenuItem.Name = "закрытьToolStripMenuItem";
+            this.закрытьToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.закрытьToolStripMenuItem.Text = "Закрыть";
+            this.закрытьToolStripMenuItem.Click += new System.EventHandler(this.закрытьToolStripMenuItem_Click);
+            // 
+            // buttonChangeOrAdd
+            // 
+            this.buttonChangeOrAdd.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonChangeOrAdd.Location = new System.Drawing.Point(203, 196);
+            this.buttonChangeOrAdd.Name = "buttonChangeOrAdd";
+            this.buttonChangeOrAdd.Size = new System.Drawing.Size(244, 23);
+            this.buttonChangeOrAdd.TabIndex = 5;
+            this.buttonChangeOrAdd.Text = "Добавить\\Изменить";
+            this.buttonChangeOrAdd.UseVisualStyleBackColor = true;
+            this.buttonChangeOrAdd.Click += new System.EventHandler(this.buttonChangeOrAdd_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(235, 24);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(178, 24);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Работа с файлами:";
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDelete.Location = new System.Drawing.Point(203, 241);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(244, 23);
+            this.buttonDelete.TabIndex = 7;
+            this.buttonDelete.Text = "Удалить данные";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 450);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonChangeOrAdd);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.treeView);
+            this.Controls.Add(this.richTextBoxFileItem);
+            this.Name = "Form1";
+            this.Text = "Проводник";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBoxFileItem;
+        private System.Windows.Forms.TreeView treeView;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripSplitButton menu;
+        private System.Windows.Forms.ToolStripMenuItem свернутьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem развернутьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem закрытьToolStripMenuItem;
+        private System.Windows.Forms.Button buttonChangeOrAdd;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonDelete;
+    }
+}
+
---
+++ WindowsFormsApp13/Form1.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp13
+{
+    public partial class Form1 : Form
+    {
+        DirectoryInfo dir;
+        public Form1()
+        {
+            InitializeComponent();
+            AddDisk();
+            
+        }
+        public void AddDisk()
+        {
+            var drive = DriveInfo.GetDrives();
+            foreach (var disk in drive)
+            {
+                TreeNode node = new TreeNode();
+                node.Tag = disk;
+                node.Text = disk.ToString();
+                node.Nodes.Add("");
+                treeView.Nodes.Add(node);
+            }
+        }
+        public FileInfo[] ReturnFileInfo(DirectoryInfo dir) => dir.GetFiles();
+        public void AddDiectory(DirectoryInfo dir, TreeNode SelectNode)
+        {
+
+            var getdir = dir.GetDirectories();
+            foreach (var d in getdir)
+            {
+                TreeNode node = new TreeNode();
+                node.Tag = d;
+                node.Text = d.ToString();
+                node.Nodes.Add("");
+                SelectNode.Nodes.Add(node);
+            }
+
+        }
+        public void AddFile(TreeNode SelectNode)
+        {
+            foreach (var d in ReturnFileInfo(dir))
+            {
+                TreeNode node = new TreeNode();
+                node.Tag = d;
+                node.Text = d.ToString();
+                node.Nodes.Add("");
+                SelectNode.Nodes.Add(node);
+            }
+        }
+
+        private void treeView_AfterExpand(object sender, TreeViewEventArgs e)
+        {
+         
+            try
+            {
+                TreeNode SelectNode = e.Node;
+                SelectNode.Nodes.Clear();
+
+                if (SelectNode.Tag is DriveInfo)
+                {
+                    DriveInfo drive = (DriveInfo)SelectNode.Tag;
+                    dir = drive.RootDirectory;
+                }
+                else
+                {
+                    dir = (DirectoryInfo)SelectNode.Tag;
+                }
+                AddDiectory(dir, SelectNode);
+                AddFile(SelectNode);
+            }
+            catch { }
+        }
+
+        private void treeView_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            
+            try
+            {
+                var selected = treeView.SelectedNode.Text;
+                richTextBoxFileItem.Text = null;
+                foreach (var el in ReturnFileInfo(dir))
+                {
+                    if (el.ToString() == selected)
+                    {
+                        using (StreamReader reader = el.OpenText())
+                        {
+                            richTextBoxFileItem.Text = reader.ReadToEnd();
+                        }
+                    }
+
+                }
+            }
+            catch { };
+
+        }
+
+        private void свернутьToolStripMenuItem_Click(object sender, EventArgs e)=>WindowState = FormWindowState.Minimized;
+
+        private void развернутьToolStripMenuItem_Click(object sender, EventArgs e)=>WindowState = FormWindowState.Maximized;
+
+        private void закрытьToolStripMenuItem_Click(object sender, EventArgs e)=>Close();
+
+        
+
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var selected = treeView.SelectedNode.Text;
+                richTextBoxFileItem.Text = null;
+                foreach (var el in ReturnFileInfo(dir))
+                {
+                    if (el.ToString() == selected)
+                    {
+                        using (StreamWriter writer = el.CreateText())
+                        {
+                            writer.WriteLine(richTextBoxFileItem.Text);
+                        }
+                    }
+
+                }
+            }
+            catch { };
+
+        }
+
+        private void buttonChangeOrAdd_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                var selected = treeView.SelectedNode.Text;
+                foreach (var el in ReturnFileInfo(dir))
+                {
+                    if (el.ToString() == selected)
+                    {
+                        using (StreamWriter writer = el.CreateText())
+                        {
+                            writer.WriteLine(richTextBoxFileItem.Text);
+                        }
+                    }
+
+                }
+            }
+            catch { };
+        }
+    }
+
+}
